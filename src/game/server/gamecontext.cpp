138f151e25530be3c3c3842e4d6feedd3242f8f0
Ryan Pettit <doorz1009@gmail.com>
2020-04-16 10:56:08 -0400

    Add ability for server to start custom gamecontroller

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -17,6 +17,7 @@
 #include "gamemodes/dm.h"
 #include "gamemodes/lms.h"
 #include "gamemodes/lts.h"
+#include "gamemodes/dbl.h"
 #include "gamemodes/mod.h"
 #include "gamemodes/tdm.h"
 #include "gamecontext.h"
@@ -1572,6 +1573,8 @@ void CGameContext::OnInit()
 		m_pController = new CGameControllerLTS(this);
 	else if(str_comp_nocase(Config()->m_SvGametype, "tdm") == 0)
 		m_pController = new CGameControllerTDM(this);
+	else if(str_comp_nocase(Config()->m_SvGametype, "dbl") == 0)
+		m_pController = new CGameControllerDBL(this);
 	else
 		m_pController = new CGameControllerDM(this);
 
