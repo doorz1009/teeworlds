73fe1fb3e566c1e4ddfbe2c0d405a51487d99f1c
Ryan Pettit <doorz1009@gmail.com>
2020-04-25 12:21:00 -0400

    End round when all players are dead on one team or time limit reached

--- src/game/server/gamemodes/dbl.cpp
+++ src/game/server/gamemodes/dbl.cpp
@@ -54,3 +54,32 @@ int CGameControllerDBL::OnCharacterDeath(class CCharacter *pVictim, class CPlaye
 	return 0;
 }
 
+void CGameControllerDBL::DoWincheckRound()
+{
+	// This function runs every server tick to check for a win condition.
+	int Count[2] = {0}; // Array for keeping track of how many players are alive on each team.
+	for(int i(0); i < MAX_CLIENTS; i++)
+	{	// 
+		if(GameServer()->m_apPlayers[i] && // Does the player at this index exist?
+		   GameServer()->m_apPlayers[i]->GetTeam() != TEAM_SPECTATORS && // Does the extant player belong to the spectator team?
+		   (!GameServer()->m_apPlayers[i]->m_RespawnDisabled || (GameServer()->m_apPlayers[i]->GetCharacter() && GameServer()->m_apPlayers[i]->GetCharacter()->IsAlive())))
+		 ++Count[GameServer()->m_apPlayers[i]->GetTeam()];	// Player exists, is not dead, and is not spectating.
+	}
+
+	if(Count[TEAM_RED]+Count[TEAM_BLUE] == 0 || (m_GameInfo.m_TimeLimit > 0 && (Server()->Tick()-m_GameStartTick) >= m_GameInfo.m_TimeLimit*Server()->TickSpeed()*60))
+	{	// If the last member of both teams dies at the same time or if the time runs out, tie.
+		++m_aTeamscore[TEAM_BLUE];
+		++m_aTeamscore[TEAM_RED];
+		EndRound();
+	}
+	else if(Count[TEAM_RED] == 0)
+	{	// Red team is completely dead, blue wins!
+		++m_aTeamscore[TEAM_BLUE];
+		EndRound();
+	}
+	else if(Count[TEAM_BLUE] == 0)
+	{	// Blue team is completely dead, red wins!
+		++m_aTeamscore[TEAM_RED];
+		EndRound();
+	}
+}
